3b4c2ab22bde700675947576e4dde21d33903a0f
mohammed <folkbs76@gmail.com>
2026-03-05 04:35:27 +0300

    Initial Commit

---
+++ sound/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sound
+//
+//  Created by mohammed on 05/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sound/soundApp.swift
@@ -0,0 +1,17 @@
+//
+//  soundApp.swift
+//  sound
+//
+//  Created by mohammed on 05/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct soundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
